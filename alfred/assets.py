f846c2086739bb6921ef9457211078be4825d625
Mike Yumatov <mike@yumatov.org>
2012-09-20 15:15:05 +0400

    Remove some confusion about collectassets command

---
+++ alfred/assets.py
@@ -0,0 +1,11 @@
+from flask_gears import Gears
+from gears_clean_css import CleanCSSCompressor
+from gears_uglifyjs import UglifyJSCompressor
+
+
+gears = Gears(
+    compressors={
+        'text/css': CleanCSSCompressor.as_handler(),
+        'application/javascript': UglifyJSCompressor.as_handler(),
+    }
+)
